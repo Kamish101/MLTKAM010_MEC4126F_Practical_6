5a2546592e5e3bdb3c91512eba7efb86e0146ee2
Kamish101 <mltkam010@myuct.ac.za>
2026-04-20 11:14:48 +0200

    UPDATE

--- src/main.c
+++ src/main.c
@@ -32,7 +32,8 @@
 // FUNCTION DECLARATIONS
 //====================================================================
 
-void helloWorld(void);
+void init_student(void);
+void init_ADC(void);
 
 //====================================================================
 // MAIN FUNCTION
@@ -40,7 +41,7 @@ void helloWorld(void);
 
 int main (void)
 {
-    helloWorld();
+    init_student();
     
     while (1)
     {
@@ -53,11 +54,32 @@ int main (void)
 // FUNCTION DEFINITIONS
 //====================================================================
 
-void helloWorld(void)
+void init_student(void)
 {
     init_LCD();
     lcd_command(CLEAR);
-    lcd_putstring("Hello world! :)");
+    lcd_putstring("MLTKAM010");
+}
+
+void init_ADC(void)
+{
+    // Enable ADC clock
+    RCC->APB2ENR |= RCC_APB2ENR_ADCEN;
+
+    ADC1 -> CFGR1 &= ~ADC_CFGR1_CONT;// Disable CONTinuous mode 
+    ADC1 -> CFGR1 |= ADC_CFGR1_DISCEN; // Enable discontinuous mode 
+    // Select ADC channel 5 (PA6)
+    ADC1->CHSELR |= ADC_CHSELR_CHSEL6;
+    // ADC1-> IER |= ADC_IER_E0
+    // Set ADC resolution to 8-bit
+    ADC1->CFGR1 |= ADC_CFGR1_RES_1;
+
+    // Enable ADC
+    ADC1->CR |= ADC_CR_ADEN;
+
+    // Wait until ADC is ready
+    while ((ADC1->ISR & ADC_ISR_ADRDY) == 0);
+    NVIC_EnableIRQ(ADC1_COMP_IRQn);
 }
 
 //====================================================================
